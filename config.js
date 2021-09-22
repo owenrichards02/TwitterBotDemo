4eea980e29cab662729353fe4a2831d193ca8088
Owen Richards <or1g20@soton.ac.uk>
2021-09-22 15:54:14 +0100

    removed config files, replaced w example

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    consumer_key:         'ZTE8M7xYAo4tPmkbtwE7fCtOh',
-    consumer_secret:      'rfI7XyvhbiOZDUqmeBrvbGG0SCx1FD2cKPr2P6O2wGHj8dHgZs',
-    access_token:         '1433812119015788567-1rF6TgtylSqKS4LrG9xkdmzbOeXbSU',
-    access_token_secret:  'e4wjyOvksxEbvrUdZUm1NEqvtuBsInorXx2RrwHnCszmy',
-    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-    strictSSL:            true,     // optional - requires SSL certificates to be valid.
-}
